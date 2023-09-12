bae86a8367ecd2eaded7ec33991865c9ac98d38e
dejandrea <dejandrea@gmail.com>
2023-09-12 20:23:25 -0300

    Aula 95 - início StoryTelling

---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import TabNavigator from "./TabNavigator";
+import Home from "../screens/Home";
+const Drawer = createDrawerNavigator();
+
+export default const DrawerNavigator = () => {
+    return (
+        <Drawer.Navigator>
+            <Drawer.Screen name="Tela Inicial" component={Home} />
+            <Drawer.Screen name="Abas" component={TabNavigator} />
+        </Drawer.Navigator>
+    );
+};
+
